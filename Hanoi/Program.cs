97843d7bc45e6f3a2c0d097ad5486bbcfebea09b
tommilostny <tommilostny@live.com>
2022-07-26 23:20:09 +0200

    Added Hanoi ;)

---
+++ Hanoi/Program.cs
@@ -0,0 +1,21 @@
+﻿Hanoi(4, "A", "B", "C");
+
+static void Move(string f, string t)
+{
+    Console.WriteLine($"Move disc from {f} to {t}!");
+}
+
+//n = number of disks
+//f = 'from' position
+//h = 'helper' position
+//t = 'target' position
+static void Hanoi(int n, string f, string h, string t)
+{
+    if (n == 0)
+    {
+        return;
+    }
+    Hanoi(n - 1, f, t, h);
+    Move(f, t);
+    Hanoi(n - 1, h, f, t);
+}
