aad6c60e6ea3c919b59b8f4c62d82589f4a56d99
tommilostny <tommilostny@live.com>
2022-09-10 15:57:29 +0200

    added quicksort

---
+++ SortingAlgorithms/Program.cs
@@ -0,0 +1,24 @@
+﻿using SortingAlgorithms;
+using System.Text.Json;
+
+var random = new Random();
+var numbers = 42;
+
+var ints = Enumerable.Range(1, numbers).Select(x => random.Next() % 100).ToArray();
+var doubles = ints.Select(x => Convert.ToDouble(x) + random.NextDouble()).ToArray();
+
+Console.WriteLine(JsonSerializer.Serialize(ints));
+Console.WriteLine();
+Console.WriteLine(JsonSerializer.Serialize(doubles));
+Console.WriteLine();
+
+Console.ReadKey();
+
+QuickSort.Sort(ints);
+QuickSort.Sort(doubles);
+
+Console.WriteLine(JsonSerializer.Serialize(ints));
+Console.WriteLine();
+Console.WriteLine(JsonSerializer.Serialize(doubles));
+
+Console.ReadKey();
---
+++ SortingAlgorithms/QuickSort.cs
@@ -0,0 +1,35 @@
+﻿namespace SortingAlgorithms;
+
+public static class QuickSort
+{
+    public static void Sort<T>(T[] array) where T : IComparable<T>
+    {
+        if (array.Length <= 1)
+            return;
+
+        var pivot = array[^1];
+        var leCount = array.Count(x => x.CompareTo(pivot) <= 0) - 1;
+
+        var lessEqual = new T[leCount];
+        var greater = new T[array.Length - leCount - 1];
+        int lei = 0, gi = 0;
+
+        for (int i = 0; i < array.Length - 1; i++)
+        {
+            if (array[i].CompareTo(pivot) <= 0)
+            {
+                lessEqual[lei++] = array[i];
+                continue;
+            }
+            greater[gi++] = array[i];
+        }
+
+        Sort(lessEqual);
+        Sort(greater);
+
+        for (int i = 0; i < array.Length; i++)
+        {
+            array[i] = i == leCount ? pivot : (i < leCount ? lessEqual[i] : greater[i - leCount - 1]);
+        }
+    }
+}
