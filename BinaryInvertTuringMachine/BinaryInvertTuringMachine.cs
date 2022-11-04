505b13d0bc0585d40b4423dc2f0f2de2082ae9a6
tommilostny <tommilostny@live.com>
2022-11-04 15:32:32 +0100

    Updated Turing machine project to be object oriented.

---
+++ BinaryInvertTuringMachine/BinaryInvertTuringMachine.cs
@@ -0,0 +1,82 @@
+﻿namespace TuringMachines;
+
+public class BinaryInvertTuringMachine : TuringMachine
+{
+    public BinaryInvertTuringMachine(string? input) : base(input, alphabet: new[]{ '0', '1' })
+    { }
+
+    public override bool Run()
+    {
+        while (_state != 7)
+        {
+            switch (_state)
+            {
+                case 1:
+                    if (_tape[_tapeIndex] == 0)
+                    {
+                        _tapeIndex++;
+                        _state = 2;
+                    }
+                    break;
+                case 2:
+                    switch (_tape[_tapeIndex])
+                    {
+                        case '0':
+                            _tape[_tapeIndex] = '1';
+                            _state = 3;
+                            break;
+                        case '1':
+                            _tape[_tapeIndex] = '0';
+                            _state = 3;
+                            break;
+                        default:
+                            _tapeIndex--;
+                            _state = 4;
+                            break;
+                    }
+                    break;
+                case 3:
+                    if (_tape[_tapeIndex] is '0' or '1')
+                    {
+                        _tapeIndex++;
+                        _state = 2;
+                    }
+                    break;
+                case 4:
+                    switch (_tape[_tapeIndex])
+                    {
+                        case '0':
+                            _tape[_tapeIndex] = '1';
+                            _state = 6;
+                            break;
+                        case '1':
+                            _tape[_tapeIndex] = '0';
+                            _state = 5;
+                            break;
+                        default:
+                            _state = 7;
+                            break;
+                    }
+                    break;
+                case 5:
+                    if (_tape[_tapeIndex] == '0')
+                    {
+                        _tapeIndex--;
+                        _state = 4;
+                    }
+                    break;
+                case 6:
+                    if (_tape[_tapeIndex] is '0' or '1')
+                    {
+                        _tapeIndex--;
+                        break;
+                    }
+                    _state = 7;
+                    break;
+
+                default: return false;
+            }
+        }
+        return true;
+    }
+}
---
+++ BinaryInvertTuringMachine/TuringMachine.cs
@@ -0,0 +1,44 @@
+﻿namespace TuringMachines;
+
+public abstract class TuringMachine
+{
+    protected uint _state = 1;
+    protected uint _tapeIndex = 0;
+    protected readonly char[] _tape;
+
+    public TuringMachine(string? input, char[] alphabet)
+    {
+        if (input is null)
+            throw new ArgumentNullException(nameof(input));
+
+        _tape = new char[input.Length + 2];
+
+        for (int j = 0; j < input.Length; j++)
+        {
+            var inputChar = input[j];
+            if (alphabet.Contains(inputChar))
+            {
+                _tape[j + 1] = inputChar;
+                continue;
+            }
+            throw new ArgumentException($"'{inputChar}' is not from given alphabet.");
+        }
+    }
+
+    /// <summary>
+    /// Run the Turing machine with state logic directly from the diagram.
+    /// Move right: i++;
+    /// Move left:  i--;
+    /// </summary>
+    /// <returns> Accept, decline. </returns>
+    public abstract bool Run();
+
+    public void PrintTape()
+    {
+        foreach (var item in _tape)
+        {
+            Console.Write(item);
+        }
+        Console.WriteLine();
+    }
+}
